c760135dfaf5c1f9ed946eecf49d05a3596acf1f
Lourdel <nlourdel62000@gmail.com>
2019-11-21 17:04:16 +0100

    new project

---
+++ ProjetEcommerce/assets/js/main.js
@@ -0,0 +1,8 @@
+var images = ["https://sf2.viepratique.fr/wp-content/uploads/sites/8/2017/04/maison-en-bois-le-permis-de-construire-est-il-obligatoire.jpg",
+"https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Maison_Gauvreau_4.JPG/1200px-Maison_Gauvreau_4.JPG",
+"https://www.construction21.org/france/data/sources/users/2548/maison-bioclimatique-passive-5.jpg"];
+
+
+$(document).ready(function(){	
+    
+});
